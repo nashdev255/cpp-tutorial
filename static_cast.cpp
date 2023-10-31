04435c25aee131f73c52d52cb61b915602937f3a
UnColored-A <finch_2nd@icloud.com>
2023-10-31 19:55:49 +0900

    Add static_cast.cpp

---
+++ static_cast.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+class Degree {
+  private:
+    int degree;
+
+  public:
+    Degree(int d = 0) : degree(d) {}
+    ~Degree() {}
+
+    void operator=(int d) {
+      degree = d;
+    }
+
+    operator int() {
+      return degree;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Degree& d) {
+      os << d.degree;
+      return os;
+    }
+
+};
+
+int main() {
+  Degree theta_0 = 30;
+  int val = theta_0;
+
+  std::cout << val << std::endl;
+  
+  return 0;
+}
+
+/*
+int main() {
+  double pi = 3.14;
+
+  // implicit cast
+  // int p = pi;
+
+  // explicit cast
+  int p = static_cast<int>(pi);
+
+  std::cout << p << std::endl;
+
+  return 0;
+}
+*/
